ef2b2de36d7eb8d9e33be222fad8cc3fa044f591
kunj0211 <patelkunj4333@gmail.com>
2026-05-06 17:20:05 +0530

    enhance | add maxlength 20 to edit and create task title

--- src/components/EditTask.jsx
+++ src/components/EditTask.jsx
@@ -89,6 +89,7 @@ const EditTask = () => {
 						{...register('title', {
 							required: 'Title is required',
 						})}
+						maxLength={20}
 						className='w-full px-3 py-2 border border-gray-300 rounded-lg shadow-sm hover:shadow-md focus:shadow-md focus:outline-none focus:ring-1 focus:ring-blue-500 focus:border-blue-500 bg-white text-gray-900 placeholder-gray-500 hover:border-blue-500 transition-all duration-300'
 					/>
 					{errors.title && (
